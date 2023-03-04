09640db078a2fb352fb9f512b3c2c676a9c2ccf0
LVAK-NapoleonII <124894637+LVAK-NapoleonII@users.noreply.github.com>
2023-03-05 00:14:52 +0700

    lab4

---
+++ lambaitap/src/lab4/Hinhhoc.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+import java.util.Scanner;
+
+public class Hinhhoc {
+  public final float Pi=3.14f;
+   public String ten;
+   public float chuVi;
+   public float dienTich;
+   public float theTich;
+    Scanner input=new Scanner(System.in);
+    public Hinhhoc(){
+        
+    }
+    public String xuatTen(String ten){
+        return ten;
+    }
+    public float inchuVi(float chuVi){
+        return chuVi;
+    }
+    public float indienTich(float dienTich){
+        return dienTich;
+    }
+    public float intheTich(float theTich){
+        return theTich;
+    }
+}
